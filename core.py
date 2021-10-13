3c71d1c75ef4fa5ba13a53e2426372e6b708b789
Adel <shi.samadel@gmail.com>
2021-10-13 20:45:41 +0300

    split into zones

--- core.py
+++ core.py
@@ -13,6 +13,8 @@ class CLAHE:
         self.lookup_tables_rect = {}
         self.numpy_control_points = []
 
+        self.center_points = np.zeros((self.nb_rects, self.nb_rects, 2))
+
     def divide_into_rect(self):
         self.height_rect = self.height // self.nb_rects
         self.width_rect = self.width // self.nb_rects
@@ -37,17 +39,24 @@ class CLAHE:
         # plt.plot(dist)
         new_dist = (dist*255).astype(np.int32)
 
-        self.lookup_tables_rect[(y_start+self.y_rect_center, x_start+self.x_rect_center)] = new_dist[self.img[y_start+self.y_rect_center, x_start+self.x_rect_center]]
+
+
+        self.lookup_tables_rect[(y_start+self.y_rect_center, x_start+self.x_rect_center)] = dist
 
         self.numpy_control_points.append([y_start+self.y_rect_center, x_start+self.x_rect_center])
         # plt.show()
 
 
     def walking_by_central_point(self):
-        for x_start in range(0, self.width, self.width_rect):
-            for y_start in range(0, self.height, self.height_rect):
+        for i, x_start in enumerate(range(0, self.width, self.width_rect)):
+            for j, y_start in enumerate(range(0, self.height, self.height_rect)):
                 if x_start+self.x_rect_center < self.width and y_start+self.y_rect_center < self.height:
                     self.get_dist_rect(x_start, y_start)
+                    center_point = [y_start+self.y_rect_center, x_start+self.x_rect_center]
+                    self.center_points[j, i] = [y_start+self.y_rect_center, x_start+self.x_rect_center]
+
+
+
         self.numpy_control_points = np.asarray(self.numpy_control_points)
 
 
@@ -55,9 +64,70 @@ class CLAHE:
         control_points = np.abs(self.numpy_control_points - np.asarray(point)).sum(axis=1)
 
         control_points = np.argsort(control_points)[:nb_nearest_points]
+        return  self.numpy_control_points[control_points]
 
 
+    def matrix_values(self):
+        matrix = np.zeros((self.height, self.width, 2))
+        for i in range(self.height):
+            for j in range(self.width):
+                matrix[i, j] = [i,j]
+        return matrix
 
+
+    def fill_each_point(self):
+
+
+        for i in range(-1,8):
+            for j in range(-1,8):
+                border = 0
+                if i == -1 and j == -1:
+                    top_left = np.array([0, 0])
+                    border = 2
+                elif i == -1:
+                    top_left = np.array([self.center_points[j-1,0][0], 0]).astype(int)
+                    border += 1
+                elif j == -1:
+                    top_left = np.array([0,self.center_points[0, i-1][1]]).astype(int)
+
+                    border += 1
+                else:
+                    top_left = self.center_points[j,i].astype(int)
+
+                    border += 0
+
+                if i == 7 and j == 7:
+                    bottom_right = np.array([self.height, self.width]).astype(int)
+
+                    border = 2
+                elif i == 7:
+                    bottom_right = np.array([self.center_points[j+1,i][0],self.width]).astype(int)
+
+                    border += 1
+                elif j == 7:
+                    bottom_right = np.array([self.height,self.center_points[j, i+1][1]]).astype(int)
+
+                    border += 1
+                else:
+                    bottom_right = self.center_points[j+1,i+1].astype(int)
+
+                    border += 0
+
+                if border == 2:
+                    zone = 'red'
+                elif border == 1:
+                    zone = 'green'
+                else:
+                    zone = 'blue'
+
+                for y in range(top_left[0], bottom_right[0]):
+                    for x in range(top_left[1], bottom_right[1]):
+                        if zone == 'blue':
+                            top_right =
+                        elif zone == 'green':
+                            self.img[y,x] = 127
+                        elif zone == 'red':
+                            self.img[y,x] = 255
 
 
 
