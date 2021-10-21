91ff270ece3d0f903e69ea4dbfd479e5e6ff73ca
Adel <shi.samadel@gmail.com>
2021-10-21 20:59:28 +0300

    final changes

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 from core import CLAHE
-import time
 
 if __name__ == '__main__':
-    alg = CLAHE('indi.png')
-    alg.show()
-    alg.divide_into_rect()
-    alg.walking_by_central_point()
-    alg.fill_each_point()
-    alg.show()
+    alg = CLAHE('imgs/xinwei1.png')
+    alg.run()
+    alg.save('result/res1.png')
